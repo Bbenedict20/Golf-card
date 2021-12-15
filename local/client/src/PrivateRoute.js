e1a28d70249180e37fc22de5cdcf1e40dae7b991
Bbenedict20 <Bradbenedict200@gmail.com>
2021-12-15 16:24:16 -0700

    update 2

--- local/client/src/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import { Route, Redirect } from 'react-router-dom';
-
-export default function PrivateRoute({ isLoggedIn, ...props }) {
-    if (isLoggedIn) {
-        return <Route {...props} />
-    } else {
-        return <Redirect to="/" />
-    }
-}
